be7b6cfe6b127e1e6df6b1e81dda3f6031c72405
devanghingu <43535635+devanghingu@users.noreply.github.com>
2020-03-30 11:35:58 +0530

    waiting model added

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Books,Category,Transaction
+from .models import Books,Category,Transaction,WaitingTransaction
 # Register your models here.
 admin.site.register(Books)
 admin.site.register(Category)
 admin.site.register(Transaction)
+admin.site.register(WaitingTransaction)
+
 
--- book/views.py
+++ book/views.py
@@ -4,7 +4,7 @@ from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from .models import Books , Transaction,WaitingTransaction
+from .models import Books, Transaction, WaitingTransaction
 
 @login_required()
 def index(request):
@@ -23,31 +23,49 @@ class IssueBookCBView(View):
     def get(self,request,*args, **kwargs):
         # check user book does not morethen five
         bookavailable=Books.objects.filter(id=kwargs['book_id'],quantity__gt=0)
-        if bookavailable.count():
-            return render(request,'book_issue.html',{'book':bookavailable.get()})
-        else:
-            return 
-        # Transaction.objects.filter()
+        try:
+            if bookavailable.count():
+                return render(request,'book_issue.html',{'book':bookavailable.get()})
+            else:
+                return render(request,'book_issue.html',{'book':bookavailable.get(),'message':'Book not Available at moment! Can you wait for book?'})
+        except Exception as e:
+            return render(request,'book_issue.html',{'message':'Book not Available at moment.! can you wait for book?'})
+        
          
     def post(self,request,*args, **kwargs):
         bookavailable=Books.objects.filter(id=kwargs['book_id'],quantity__gt=0)
         book=request.POST.get("bookid", "")
-        print(book)
-        if bookavailable.count():
-            # check book already taken or not 
-            bookexist=Transaction.objects.filter(issue_by=request.user,return_date=None,book=bookavailable.get())
-            if bookexist:
-                messages.warning(request,"book already issue by you. one book cannot issue multiple at a times ")
+        try:
+            if bookavailable.count():
+                 
+                bookexist=Transaction.objects.filter(issue_by=request.user,return_date=None,book=bookavailable.get())
+                if bookexist:
+                    messages.warning(request,"book already issue by you. one book cannot issue multiple at a times ")
+                    return redirect('books')
+                bookavailable=bookavailable.get()
+                Transaction.objects.create(book=bookavailable,issue_by=request.user)
+                bookavailable.quantity-=1
+                bookavailable.save()
+                messages.success(request,"book has been issue done")
                 return redirect('books')
-            bookavailable=bookavailable.get()
-            Transaction.objects.create(book=bookavailable,issue_by=request.user)
-            bookavailable.quantity-=1
-            bookavailable.save()
-            messages.success(request,"book has been issue done")
-            return redirect('books')
-        else:
-            # waiting model
-            return 0
+            else:
+                book1=Books.objects.filter(id=kwargs['book_id']).get()
+                transexist=WaitingTransaction.objects.filter(book=book1,issue_by=request.user).get()
+                if transexist:
+                    messages.error(request,'opps you are already in waiting queue')
+                    return redirect('mybooks')
+                WaitingTransaction.objects.create(book=book1,issue_by=request.user)
+                messages.success(request,"Your request added in queue.")
+                return redirect('mybooks')
+        except Exception as e:
+            book1=Books.objects.filter(id=kwargs['book_id']).get()
+            transexist=WaitingTransaction.objects.filter(book=book1,issue_by=request.user).get()
+            if transexist:
+                messages.error(request,'opps you are already in waiting queue')
+                return redirect('mybooks')
+            WaitingTransaction.objects.create(book=book1,issue_by=request.user)
+            messages.success(request,"Your request added in queue.")
+            return redirect('mybooks')
 class MybookCBV(View):
     def get(self,request):
         allbook=Transaction.objects.filter(issue_by=request.user,return_date=None)
@@ -105,3 +123,21 @@ class PastTransactionCBV(View):
         transation_exist= Transaction.objects.filter(issue_by=request.user, return_date__isnull=False)
         if transation_exist:
             return render(request,'book_return_transaction.html',{'tran':transation_exist})
+
+class MybookWaitingCBView(View):
+    def get(self,request,*args, **kwargs):
+        # get all books of user
+        books=WaitingTransaction.objects.filter(issue_by=request.user)
+        # waitinguser=[]
+        # for book1 in books:
+        #     waitinguser.append(WaitingTransaction.objects.filter(book=book1.book).order_by('issue_date'))
+        # print(waitinguser)    
+        return render(request,'book_user_waiting.html',{'books':books})
+    def post(self,request,*args, **kwargs):
+        pass
+
+class UserWaitingList(View):
+    def get(self,request,*args, **kwargs):
+        book1=WaitingTransaction.objects.filter(id=kwargs['book_id']).get()
+        waitinguser=WaitingTransaction.objects.filter(book=book1.book).order_by('issue_date')
+        return render(request,'book_user_waiting_show.html',{'books':waitinguser})
